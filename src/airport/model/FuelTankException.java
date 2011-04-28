deb0fb4ca06cea7cfce652bbb77a4ec9d46119b1
Jeremy Allen <jma.8484@gmail.com>
2011-04-28 16:02:15 -0500

    v3 removed all extraneous reconstrutables, eg: javadoc, maven, vpproject, etc...

---
+++ src/airport/model/FuelTankException.java
@@ -0,0 +1,33 @@
+/**
+ * @project AirportSimulator
+ * @package airport.model
+ * @author Brian Bagley
+ * @author David Cook
+ * @author Jeremy Allen
+ * @author Joshua Charles
+ * @version 3.0
+ */
+package airport.model;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class FuelTankException.
+ * @author Jeremy Allen
+ * @version 3.0
+ */
+public class FuelTankException extends Exception {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1776886393936062448L;
+
+	/**
+	 * Instantiates a new fuel tank exception.
+	 * 
+	 * @param s
+	 *            the s
+	 */
+	public FuelTankException(String s) {
+		super(s);
+	}
+
+}
