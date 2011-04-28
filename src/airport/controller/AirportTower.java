52e1db402f3b40d6869593b74c0149e8abd734f2
Jeremy Allen <jma.8484@gmail.com>
2011-04-28 16:06:54 -0500

    v3 final shorter package names

---
+++ src/airport/controller/AirportTower.java
@@ -0,0 +1,89 @@
+/**
+ * @project AirportSimulator
+ * @package AirportSimulator.Controller
+ * @author Brian Bagley
+ * @author David Cook
+ * @author Jeremy Allen
+ * @author Joshua Charles
+ * @version 3.0
+ */
+package airport.controller;
+
+import airport.model.*;
+import airport.view.*;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class AirportTower.
+ * @author Joshua Charles
+ * @author Jeremy Allen
+ * @version 3.0
+ */
+public final class AirportTower extends AbstractController {
+
+	/**
+	 * Instantiates a new airport tower.
+	 * 
+	 * @param args
+	 *            the args
+	 */
+	public AirportTower(String[] args) {
+		setModel(new AirportModel());
+		setView(new AirportView((AirportModel) getModel(), this));
+		((JFrameView) getView()).setVisible(true);
+	}
+
+	/**
+	 * Operation.
+	 * 
+	 * @param actionCommand
+	 *            the action command
+	 */
+	public void operation(String actionCommand) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Pause.
+	 */
+	public void pause() {
+		((AirportModel) getModel()).pause();
+	}
+
+	/**
+	 * Resume.
+	 */
+	public void resume() {
+		((AirportModel) getModel()).resume();
+	}
+
+	/**
+	 * Sets the speed.
+	 * 
+	 * @param current
+	 *            the new speed
+	 */
+	public void setSpeed(int current) {
+		((AirportModel) getModel()).setSpeed(current);
+	}
+
+	/**
+	 * Start.
+	 * 
+	 * @param listsize
+	 *            the listsize
+	 * @param arrivalrate
+	 *            the arrivalrate
+	 */
+	public void start(int listsize, double arrivalrate) {
+		((AirportModel) getModel()).start(listsize, arrivalrate);
+	}
+
+	/**
+	 * Stop.
+	 */
+	public void stop() {
+		((AirportModel) getModel()).stop();
+	}
+}
---
+++ src/airport/model/FuelLevel.java
@@ -0,0 +1,154 @@
+/**
+ * @project AirportSimulator
+ * @package airport.model
+ * @author Brian Bagley
+ * @author David Cook
+ * @author Jeremy Allen
+ * @author Joshua Charles
+ * @version 3.0
+ */
+package airport.model;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Enum FuelLevel.
+ * @author Jeremy Allen
+ * @version 3.0
+ */
+public enum FuelLevel {
+
+
+	/** The ERROR. */
+	ERROR("Error", -1),
+	/** The EMPTY. */
+	EMPTY("Empty", 0),
+	/** The DANGER. */
+	DANGER("Dangerous", 10),
+
+	/** The EMERGENCY. */
+	EMERGENCY("Emergency", 25),
+
+	/** The LOW. */
+	LOW("Low", 30),
+
+	/** The MEDIUM. */
+	MEDIUM("Medium", 50),
+
+	/** The HIGH. */
+	HIGH("High", 75),
+
+	/** The FULL. */
+	FULL("Full", 95),
+
+	/** The MAX. */
+	MAX("Maximum", 100);
+
+
+	/**
+	 * Gets the level.
+	 * 
+	 * @param s
+	 *            the s
+	 * @return the level
+	 */
+	static FuelLevel getLevel(String s) {
+		return FuelLevel.valueOf(s);
+	}
+
+	// Classify fuel level to a String
+	/**
+	 * Gets the level name.
+	 * 
+	 * @param amt
+	 *            the amt
+	 * @return the level name
+	 */
+	static String getLevelName(int amt) {
+		return ((amt == 100) ? "Max"
+				: (amt > 95) ? "Full"
+				: (amt > 70) ? "High"
+				: (amt > 50) ? "Medium"
+				: (amt > 25) ? "Low"
+				: (amt > 10) ? "Emergency"
+				: (amt > 0) ? "Dangerous"
+				: (amt == 0) ? "Empty"
+																										: "Error");
+	}
+	
+	/**
+	 * Gets the level.
+	 *
+	 * @param amt the amt
+	 * @return the level
+	 */
+	static FuelLevel getLevel(int amt) {
+		return ((amt == 100) ? MAX
+				: (amt > 95) ? FULL
+				: (amt > 70) ? HIGH
+				: (amt > 50) ? MEDIUM
+				: (amt > 25) ? LOW
+				: (amt > 10) ? EMERGENCY
+				: (amt > 0) ? DANGER
+				: (amt == 0) ? EMPTY
+				: ERROR);
+	}
+
+	/** The multiplier. */
+	int multiplier;
+
+	/** The name. */
+	String name;
+
+	/**
+	 * Instantiates a new fuel level.
+	 * 
+	 * @param amt
+	 *            the amt
+	 */
+	FuelLevel(int amt) {
+		this.name = getLevelName(amt);
+		this.multiplier = amt;
+	}
+
+	/**
+	 * Instantiates a new fuel level.
+	 * 
+	 * @param s
+	 *            the s
+	 */
+	FuelLevel(String s) {
+		this.name = s;
+		multiplier = FuelLevel.valueOf(s).multiplier;
+	}
+
+	/**
+	 * Instantiates a new fuel level.
+	 * 
+	 * @param s
+	 *            the s
+	 * @param amt
+	 *            the amt
+	 */
+	FuelLevel(String s, int amt) {
+		this.name = s;
+		this.multiplier = amt;
+	}
+
+	/**
+	 * Gets the level.
+	 * 
+	 * @return the level
+	 */
+	FuelLevel getLevel() {
+		return this;
+	}
+
+	/**
+	 * Multiplier.
+	 * 
+	 * @return the double
+	 */
+	public double multiplier() {
+		return multiplier / 100.0;
+	}
+}
